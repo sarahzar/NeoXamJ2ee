cc463bf4cc2f088ad9dcf8649a47d4c3e15cc958
Tijani <Tijani@HP-Laptop.home>
2019-07-03 18:31:41 +0100

    Ajout de la classe RestActivator dans le package "Utils"

---
+++ NeoXamHR-ejb/src/main/java/dev/project/utils/RestActivator.java
@@ -0,0 +1,10 @@
+package dev.project.utils;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestActivator extends Application{
+	
+
+}
