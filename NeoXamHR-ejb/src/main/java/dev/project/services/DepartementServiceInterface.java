3ead62d5c50c2a0ed81a017636a16eb65134ce9b
Tijani <Tijani@HP-LAPTOP>
2019-07-16 23:49:16 +0100

    Ajout de LanguageRessources

--- NeoXamHR-ejb/src/main/java/dev/project/services/DepartementServiceInterface.java
+++ NeoXamHR-ejb/src/main/java/dev/project/services/DepartementServiceInterface.java
@@ -1,12 +1,7 @@
 package dev.project.services;
 
-import java.util.List;
-
 import javax.ejb.Local;
-import javax.ejb.Remote;
-
 import dev.project.dao.DepartementDaoInterface;
-import dev.project.entities.Departement;
 
 @Local
 public interface DepartementServiceInterface extends DepartementDaoInterface{
